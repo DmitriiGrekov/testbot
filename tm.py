5d67a597d3b1f164bb0cea48ce2681eb691ef5cb
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-31 16:27:53 +0900

    new

--- tm.py
+++ tm.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types
 token="889958255:AAFx0HHiWKr1qgcjA5jOYLsW_d84gxiKZ7U"
 persons={}
+bot=telebot.TeleBot(token)
 
 @bot.message_handler(command=["start",'help'])
 def hand_mes(message):
