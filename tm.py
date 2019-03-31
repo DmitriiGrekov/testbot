9fa6295c1ff53483a6973e6cf3c1408ffbd2beeb
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-31 19:12:58 +0900

    'news'

--- tm.py
+++ tm.py
@@ -4,19 +4,24 @@ token="889958255:AAFx0HHiWKr1qgcjA5jOYLsW_d84gxiKZ7U"
 persons={}
 bot=telebot.TeleBot(token)
 
+def create_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Переводчик",request_location=True)
+    button_phone = types.KeyboardButton(text="/Назад")
+    keyboard.add(button_phone)
+    return keyboard
 @bot.message_handler(commands=["start",'help'])
 def hand_mes(message):
      
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Переводчик")
-    
-    keyboard.add(button_phone)
+    keyboard=create_keyboard()
     send=bot.send_message(message.chat.id, "Выбериет функцию", reply_markup=keyboard)
     bot.register_next_step_handler(send,second)
 def second(message):
-    global persons
-    if message.text.lower() == "переводчик":
-        persons[message.chat.id]=['переводчик',"23"]
-    bot.send_message(message.chat.id,persons[message.chat.id])
+    if message.text == 'Переводчик':
+        lon=message.location.longtitude
+        lan=message.location.lattitude
+        bot.send_message(message.chat.id,"Широта "+lon+"Долгота "+lan)
+    
+        
 
 bot.polling()
