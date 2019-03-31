5417d78a545c325b7420a615b864e77deb17b09e
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-31 16:37:55 +0900

    new

--- tm.py
+++ tm.py
@@ -6,6 +6,14 @@ bot=telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start",'help'])
 def hand_mes(message):
-    bot.send_message(message.chat.id,'Hello')
+     Эти параметры для клавиатуры необязательны, просто для удобства
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Переводчик")
+    
+    keyboard.add(button_phone)
+    send=bot.send_message(message.chat.id, "Выбериет функцию", reply_markup=keyboard)
+    bot.register_next_step_handler(send,second)
+def second(message):
+    bot.send_message(message.chat.id,"Вы отправили -> " +message.text)
 
 bot.polling()
