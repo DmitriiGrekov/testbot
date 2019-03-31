5a5bf04a501e6082194850efaafbc5ddf669b90b
DmitriiGrekov <grekovdima7@gmail.com>
2019-03-31 16:32:06 +0900

    new

--- tm.py
+++ tm.py
@@ -6,16 +6,6 @@ bot=telebot.TeleBot(token)
 
 @bot.message_handler(command=["start",'help'])
 def hand_mes(message):
-
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Переводчик")
-    
-    keyboard.add(button_phone)
-    send=bot.send_message(message.chat.id, "Выберите функцию", reply_markup=keyboard)
-    bot.register.next_step_handler(send,second)
-def second(message):
-    if message.text.lower == 'переводчик':
-        persons[message.chat.id]=['переводчик']
-    print(persons)
+    bot.send_message(message.chat.id,'Hello')
 
 bot.polling()
