df15d8712668a4fd9764150f5430124b0e747bb4
DmitriiGrekov <grekovdima7@gmail.com>
2019-04-01 14:11:03 +0900

    news

--- tm.py
+++ tm.py
@@ -10,7 +10,7 @@ bot=telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def handle_message(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True,one_time_keyboard : True) #Активация, название, количество кнопок по одной в ряду 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard : True) #Активация, название, количество кнопок по одной в ряду 
     itembtn1 = types.KeyboardButton('Переводчик') #Название кнопки 1 
     
     markup.add(itembtn1)
