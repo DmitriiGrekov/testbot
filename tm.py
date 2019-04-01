25ef3d5b25f235588db5f1bad4769a4c3b9b5a12
DmitriiGrekov <grekovdima7@gmail.com>
2019-04-01 21:18:19 +0900

    'news3'

--- tm.py
+++ tm.py
@@ -61,25 +61,30 @@ def handle_lang2(message):
     
 @bot.message_handler(func=lambda message:get_state(message)== RESULT)
 def translate(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #Активация, название, количество кнопок по одной в ряду 
-    itembtn1 = types.KeyboardButton('/start') #Название кнопки 1
+    if message.text == "назад":
+        update_state(message,START)
+        bot.send_message(message.chat.id,"Возвращаюсь в меню")
+    else:
+        
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #Активация, название, количество кнопок по одной в ряду 
+        itembtn1 = types.KeyboardButton('/start') #Название кнопки 1
     
     
-    markup.add(itembtn1)
+        markup.add(itembtn1)
     
     
         
-    langer=get_lang(message.chat.id)
-    lang1=langer["lang1"]
-    lang2=langer["lang2"]
+        langer=get_lang(message.chat.id)
+        lang1=langer["lang1"]
+        lang2=langer["lang2"]
     
-    url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
-    key='trnsl.1.1.20190201T172728Z.34034e93ef318814.4cd85f71122011aa48770690493d232d5ff78c60'    
-    TEXT=message.text
-    LANg=lang1+"-"+lang2
-    r=requests.post(url,data={'key':key,'text':TEXT,'lang':LANg})
-    bot.send_message(message.chat.id,*eval(r.text)['text'])
-    bot.send_message(message.chat.id,"Введите фразу",reply_markup=markup)
+        url='https://translate.yandex.net/api/v1.5/tr.json/translate?'
+        key='trnsl.1.1.20190201T172728Z.34034e93ef318814.4cd85f71122011aa48770690493d232d5ff78c60'    
+        TEXT=message.text
+        LANg=lang1+"-"+lang2
+        r=requests.post(url,data={'key':key,'text':TEXT,'lang':LANg})
+        bot.send_message(message.chat.id,*eval(r.text)['text'])
+        bot.send_message(message.chat.id,"Введите фразу",reply_markup=markup)
     
  
 @bot.message_handler(func=lambda message:get_state(message)== TEST)
