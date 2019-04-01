c1801b9721fc4ca57dd1afe455947a920b57a26b
DmitriiGrekov <grekovdima7@gmail.com>
2019-04-01 12:57:28 +0900

    news

--- tm.py
+++ tm.py
@@ -1,69 +1,26 @@
 import telebot
 import sqlite3
 from telebot import types
+from collections import defaultdict
 token="889958255:AAFx0HHiWKr1qgcjA5jOYLsW_d84gxiKZ7U"
+START,TRANSLATED,LANG1,LANG2=range(4)
+bot=telebot.TeleBot(token)    
+USER_STATE=defaultdic(lambda:START)
 
-bot=telebot.TeleBot(token)
-conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
-cursor = conn.cursor()
-    
-# Создание таблицы
-cursor.execute("""CREATE TABLE langer
-                  (id text, lang text)
-               """)
-cursor.execute("INSERT INTO langer VALUES (?,?)", [message.chat.id,"-"])
-def create_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Переводчик")
-    
-    keyboard.add(button_phone)
-    return keyboard
-@bot.message_handler(commands=["start",'help'])
-def hand_mes(message):
-    
-    keyboard=create_keyboard()
-    send=bot.send_message(message.chat.id, "Выбериет функцию", reply_markup=keyboard)
-    bot.register_next_step_handler(send,second)
-    
-    
-def second(message):
-    
-    
+def get_state(message):
+    return USER_STATE[message.chat.id]
+def update_state(message,state):
+    USER_STATE[message.chat.id]=state
 
-    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    butt_1 = types.KeyboardButton(text="rus-eng")
-    butt_2 = types.KeyboardButton(text="eng-rus")
-    button_back = types.KeyboardButton(text="/Назад")
-    keyboard.add(butt_1,butt_2,button_back)
-    send=bot.send_message(message.chat.id,"Введите язык",reply_markup=keyboard)
-    bot.register_next_step_handler(send,third)
-def third(message):
-    if message.text == "/Назад":
-        
-        conn = sqlite3.connect('mydatabase.db')
-        c = conn.cursor()
+@bot.message_handler(func=lambda message: get_state(message) == START)
+def handle_message(message):
+    bot.send_message(message.chat.id,"Выберите функцию")
+    if message.text.lower() == "переводчик":
+        update_state(message,TRANSLATED)
 
-        query = "DELETE FROM langer WHERE id = '%s'" %(message.chat.id)
-        
-
-        mydata = c.execute(query)
-        conn.commit()
-        keyboard=create_keyboard()
-        bot.send_message(message.chat.id,"Выберите функцию",reply_markup=keyboard)
-        return 1
-    
-    conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
-    cursor = conn.cursor()
-    query="UPDATE albums SET lang = '%s' WHERE id = '%s'"%(message.text,message.chat.id)
-    
-    sql = "SELECT * FROM langer WHERE id=?"
-    cursor.execute(sql, [(message.chat.id)])
-    bot.send_message(message.chat.id,cursor.fetchall())
-    conn.commit() 
-    
-
-
-    
+@bot.message_handler(func=lambda message: get_state(message) == TRANSLATED)
+def handle_lang(message):
+    bot.send_message(message.chat.id,"Выберите первый язык")
 
     
         
